2b0aeb87825523a511692bc0f8fa4f3310a1ef18
Benjamin Krein <benjamin.krein@ibm.com>
2018-09-28 08:03:24 -0400

    initial code

---
+++ skbot.go
@@ -0,0 +1,106 @@
+/*
+ * Borrowed from this blog post: https://rsmitty.github.io/Slack-Bot/
+ */
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"os"
+	"strings"
+
+	"github.com/nlopes/slack"
+)
+
+type fn func() string
+
+func main() {
+
+	token := os.Getenv("SKBOT_API_TOKEN")
+	api := slack.New(token)
+	api.SetDebug(true)
+
+	rtm := api.NewRTM()
+	go rtm.ManageConnection()
+
+Loop:
+	for {
+		select {
+		case msg := <-rtm.IncomingEvents:
+			fmt.Print("Event Received: ")
+			switch ev := msg.Data.(type) {
+			case *slack.ConnectedEvent:
+				fmt.Println("Connection counter:", ev.ConnectionCount)
+
+			case *slack.MessageEvent:
+				fmt.Printf("Message: %v\n", ev)
+				info := rtm.GetInfo()
+				prefix := fmt.Sprintf("<@%s> ", info.User.ID)
+
+				if ev.User != info.User.ID && strings.HasPrefix(ev.Text, prefix) {
+					respond(rtm, ev, prefix)
+				}
+
+			case *slack.RTMError:
+				fmt.Printf("Error: %s\n", ev.Error())
+
+			case *slack.InvalidAuthEvent:
+				fmt.Printf("Invalid credentials")
+				break Loop
+
+			default:
+				//Take no action
+			}
+		}
+	}
+}
+
+func respond(rtm *slack.RTM, msg *slack.MessageEvent, prefix string) {
+	var response string
+	text := msg.Text
+	text = strings.TrimPrefix(text, prefix)
+	text = strings.TrimSpace(text)
+	text = strings.ToLower(text)
+
+	acceptedGreetings := map[string]bool{
+		"what's up?": true,
+		"hey!":       true,
+		"yo":         true,
+	}
+	acceptedHowAreYou := map[string]bool{
+		"how's it going?": true,
+		"how are ya?":     true,
+		"feeling okay?":   true,
+	}
+
+	if acceptedGreetings[text] {
+		response = "What's up buddy!?!?!"
+		rtm.SendMessage(rtm.NewOutgoingMessage(response, msg.Channel))
+	} else if acceptedHowAreYou[text] {
+		response = "Good. How are you?"
+		rtm.SendMessage(rtm.NewOutgoingMessage(response, msg.Channel))
+	} else {
+		// Figure out how to return my public IP
+		response = commandFunc(text)
+		rtm.SendMessage(rtm.NewOutgoingMessage(response, msg.Channel))
+	}
+}
+
+func commandFunc(f func() string) string {
+	return f()
+}
+
+func getInternetIp() string {
+	resp, err := http.Get("http://ifconfig.co")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer resp.Body.Close()
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
+	return "Your Internet IP is: " + string(body)
+}
